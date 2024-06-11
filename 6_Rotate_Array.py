7ed9a112f102c25653abb519b8e0a4466a05142f
SumDeusVitae <kyrylosorokovskyi@gmail.com>
2024-06-11 12:18:18 -0500

    ref

--- 6_Rotate_Array.py
+++ 6_Rotate_Array.py
@@ -6,9 +6,6 @@ class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
         """
         Do not return anything, modify nums in-place instead.
-        """
-        i: int = 0
-        while i < k:
+        """ 
+        for i in range(k):
             nums.insert(0,nums.pop(-1))
-            i+=1
-        
